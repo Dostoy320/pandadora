9843abc6ac01c818a056ab9b8e2a46124e1b8a12
root <root@raspberrypi>
2013-12-04 08:02:59 -0600

    Initial commit - rough draft of a Pandora track scraper.

---
+++ pandadora.js
@@ -0,0 +1,21 @@
+scrape1 = document.getElementsByClassName('trackData');
+scrape2 = scrape1.item(0).children;
+albumString = scrape2.item(0).children.item(0);
+fullAlbumArray = albumString.href.split('/');
+
+var track = fullAlbumArray.pop();
+var album = fullAlbumArray.pop();
+var artist = fullAlbumArray.pop();
+
+statement = "Artist is: " + artist + "\n" + "Album is: " + album + "\n" + "Track is: " + track;
+
+alert(statement);
+
+
+
+// Code for bookmarklet
+//javascript: function killerapp() {
+	//var jsCode = document.createElement('script');
+	//jsCode.setAttribute('src', 'http://50.137.252.151/pandadora/pandadora.js');
+	//document.body.appendChild(jsCode);
+//} killerapp();
